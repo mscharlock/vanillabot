e0c8602d54fe934cda73498fb28f1a67c2394533
mscharlock <michelle.scharlock@gmail.com>
2018-01-22 22:40:01 -0800

    First level of CLI options working, need to figure out how to do nested commands?

---
+++ app.js
@@ -0,0 +1,75 @@
+'use strict';
+//requiring in commander, which will make it easier to slice/dice args
+const program = require('commander');
+
+//coming up with a test array for drones to see if I can get information to return
+const categories = ['drones', 'gaming', 'phones', 'computers', 'home', 'wearable'];
+const dronesArr = ['droneA', 'droneB', 'droneC'];
+const dronePrices = [14, 99, 200];
+
+console.log('Please enter what you are interested in. Options include drones (-d), gaming (-g), & wearables (-w)');
+//Options for our program
+program
+  .version('0.1.0')
+  .option('-d, --drones', 'Explore what drones we have available')
+  .option('-g, --gaming', 'Explore what gaming systems we have available')
+  .option('-w, --wearable', 'Explore which wearables we have available')
+  .option('-o, --one', 'Ok, you have chosen ' + dronesArr[0])
+  .option('-t, --two', 'Ok, you have chosen ' + dronesArr[1])
+  .option('-th, --three', 'Ok, you have chosen ' + dronesArr[2])
+  .parse(process.argv);
+
+if (program.drones) console.log('Ok, getting info on drones. We have ' +dronesArr[0] +' or ' +dronesArr[1] + ' or ' + dronesArr[2] + '. To choose one, use one(-o), two(-t), or three(-th) for your choice');
+if (program.gaming) console.log('Ok, getting info on gaming');
+if (program.wearable) console.log('Ok, getting info on wearables');
+
+if (program.one) console.log('Ok, you have chosen ' + dronesArr[0] + '. It costs ' +dronePrices[0] + ' per month');
+if (program.two) console.log('Ok, you have chosen ' + dronesArr[1]);
+if (program.three) console.log('Ok, you have chosen ' + dronesArr[2]);
+
+
+
+//using the demo from commander
+// vanillaBot
+//   .version('0.1.0')
+//   .option('-d, --drones', 'Explore which drones we have available')
+//   .option('-g, --gaming', 'Explore which gaming and VR systems are available')
+//   .option('-w, --wearable', 'Explore our wearable options!')
+//   .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+//   .parse(process.argv);
+//
+// console.log('you ordered a pizza with:');
+// if (program.peppers) console.log('  - peppers');
+// if (program.pineapple) console.log('  - pineapple');
+// if (program.bbqSauce) console.log('  - bbq');
+// console.log('  - %s cheese', program.cheese);
+
+
+//from: https://medium.com/@thatisuday/developing-node-js-cli-app-module-package-with-command-line-flags-and-sub-commands-using-commander-de0af87be7c1
+// #!/usr/bin/env node
+// const program = require('commander');
+// program
+// .option('-p, --pepper', 'adds pepper to the pizza')
+// .option('-c, --cheese', 'adds pepper to the pizza')
+// .option('-h, --hot-level <hot>', 'select hotness level of pizza', 'not that')
+// .parse(process.argv);
+//
+// if(program.pepper)   console.log('Your pizza contains pepper');
+// if(program.cheese)   console.log('Your pizza contains cheese');
+// if(program.hotLevel) console.log('You pizza is ' + program.hotLevel + ' hot');
+
+
+// an idea about using readline in node
+// const readline = require('readline');
+//
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+//
+// rl.question('What do you think of Node.js? ', (answer) => {
+//   // TODO: Log the answer in a database
+//   console.log(`Thank you for your valuable feedback: ${answer}`);
+//
+//   rl.close();
+// });
---
+++ data.js
@@ -0,0 +1,135 @@
+//Data for Grover items
+const groverData = {
+  'phones & tablets': {
+    'item1': {
+      'name': 'iPhone 7 128GB',
+      'brand': 'Apple',
+      'price': 44.99
+    },
+    'item2': {
+      'name': 'iPhone 7 32GB',
+      'brand': 'Apple',
+      'price': 39.99
+    },
+    'item3': {
+      'name': 'iPhone 7 Plus 32GB',
+      'brand': 'Apple',
+      'price': 49.99
+    },
+    'item4': {
+      'name': 'Galaxy S8 64GB',
+      'brand': 'Samsung',
+      'price': 49.99
+    },
+  },
+  'drones': {
+    'item5': {
+      'name': 'Drone BEBOP',
+      'brand': 'Parrot',
+      'price': 49.99
+    },
+    'item6': {
+      'name': 'Drone BEBOP 2',
+      'brand': 'Parrot',
+      'price': 59.99
+    },
+  },
+  'gaming & VR': {
+    'item8': {
+      'name': 'Vive',
+      'brand': 'HTC',
+      'price': 59.99,
+    },
+    'item9': {
+      'name': 'Virtual Reality Glasses Rift VR',
+      'brand': 'Occulus',
+      'price': 59.99,
+    },
+  },
+  'computing': {
+    'item10': {
+      'name': 'MacBook 12" M-5Y31, 8GB RAM, 516GB',
+      'brand': 'Apple',
+      'price': 59.99,
+    },
+    'item11': {
+      'name': 'MacBook Air 11" i7 2.2, 8GB RAM, 512GB',
+      'brand': 'Apple',
+      'price': 64.99,
+    },
+    'item12': {
+      'name': 'MacBook Air 13" i5-5250U, 4GB RAM, 128GB',
+      'brand': 'Apple',
+      'price': 69.99,
+    },
+    'item13': {
+      'name': 'MacBook Pro 13" i5-3210M, 4GB RAM, 500GB',
+      'brand': 'Apple',
+      'price': 74.99,
+    },
+    'item14': {
+      'name': 'Convertible Laptop Surface Book 512GB SSD Intel Core i7 16GB RAM dGPU',
+      'brand': 'Microsoft',
+      'price': 59.99,
+    },
+    'item15': {
+      'name': 'Convertible Laptop YOGA 300-11IBR 80M1004KGE',
+      'brand': 'Lenovo',
+      'price': 59.99,
+    },
+  },
+  'wearables':{
+    'item16': {
+      'name': 'Watch 38mm',
+      'brand': 'Apple',
+      'price': 39.99,
+    },
+    'item17': {
+      'name': 'Watch 42mm',
+      'brand': 'Apple',
+      'price': 44.99,
+    },
+    'item18': {
+      'name': 'Watch Ambit 3',
+      'brand': 'Suunto',
+      'price': 39.99,
+    },
+    'item19': {
+      'name': 'Watch V800',
+      'brand': 'Polar',
+      'price': 39.99,
+    },
+    'item20': {
+      'name': 'Watch WI503Q-1LDBR0001',
+      'brand': 'Asus',
+      'price': 44.99,
+    },
+  },
+  'smart home': {
+    'item21': {
+      'name': 'Alexa Dot',
+      'brand': 'Amazon',
+      'price': 44.99,
+    },
+    'item22': {
+      'name': 'Alexa Echo',
+      'brand': 'Amazon',
+      'price': 49.99,
+    },
+    'item23': {
+      'name': 'Qbo Milk Master',
+      'brand': 'Tchibo',
+      'price': 29.99,
+    },
+    'item24': {
+      'name': 'Robotic Vacuum Cleaner POWERbot VR20J9020UR/EG',
+      'brand': 'Samsung',
+      'price': 39.99,
+    },
+    'item25': {
+      'name': 'Robotic Vacuum Cleaner POWERbot VR20J9259U/EG',
+      'brand': 'Samsung',
+      'price': 39.99,
+    },
+  }
+};
